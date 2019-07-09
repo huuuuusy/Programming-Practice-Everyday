b9596de55e5b7296e1711921cf2c6ef3b7bffc78
huuuuusy <hushiyu1995@qq.com>
2019-07-09 15:48:19 +0800

    190709

---
+++ LeetCode/0019-删除链表的倒数第N个节点/V1.py
@@ -0,0 +1,29 @@
+"""
+@Author: huuuuusy
+@GitHub: https://github.com/huuuuusy
+系统： Ubuntu 18.04
+IDE:  VS Code 1.36
+工具： python == 3.7.3
+"""
+
+"""
+思路:
+    
+结果：
+    执行用时 : 44 ms, 在所有 Python3 提交中击败了92.35%的用户
+    内存消耗 : 18.1 MB, 在所有 Python3 提交中击败了32.48%的用户
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
