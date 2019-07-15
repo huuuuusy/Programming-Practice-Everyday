3166217ce4ed67a49d0ff912bc20f658ffdb2747
huuuuusy <hushiyu1995@qq.com>
2019-07-15 09:45:53 +0800

    190715字典方式，可以通过但是时空复杂度不行

---
+++ LeetCode/0219-存在重复元素 II/V1.py
@@ -0,0 +1,40 @@
+""" 
+@Author: huuuuusy
+@GitHub: https://github.com/huuuuusy
+系统： Ubuntu 18.04
+IDE:  VS Code 1.36
+工具： python == 3.7.3
+"""
+
+"""
+思路:
+    用字典存储下标，然后检查下标之间的最小差值是否不超过k
+    时间空间复杂度不行，可以考虑双指针＋滑动窗口优化
+结果：
+    执行用时 : 76 ms, 在所有 Python3 提交中击败了35.01%的用户
+    内存消耗 : 23.8 MB, 在所有 Python3 提交中击败了5.06%的用户
+"""
+
+class Solution:
+    def containsNearbyDuplicate(self, nums, k):
+        d = {}
+        # 用字典存储下标
+        for i in range(len(nums)):
+            d[nums[i]] = d.get(nums[i],[]) + [i]
+        for key,value in d.items():
+            if len(value) == 1:
+                continue
+            else:
+                # 检查下标之间的最小差值是否不超过k
+                dist = []
+                for i in range(len(value)-1):
+                    dist.append(value[i+1]-value[i])
+                if min(dist) <= k:
+                    return True
+        return False
+
+if __name__ == "__main__":
+    nums = [1,2,3,1]
+    k = 3
+    answer = Solution().containsNearbyDuplicate(nums, k)
+    print(answer)
