ce4099bbf2766e6a656d5400728e7b656003988e
huuuuusy <hushiyu1995@qq.com>
2019-11-06 19:23:07 +0800

    191106

---
+++ LeetCode Python/0661-图片平滑器/V1.py
@@ -0,0 +1,23 @@
+""" 
+@Author: huuuuusy
+@GitHub: https://github.com/huuuuusy
+系统： Ubuntu 18.04
+IDE:  VS Code 1.39
+工具： python == 3.7.3
+"""
+
+"""
+思路:
+    简单的方法，无难度
+结果：
+    执行用时 : 92 ms, 在所有 Python3 提交中击败了36.03%的用户
+    内存消耗 : 14.1 MB, 在所有 Python3 提交中击败了5.32%的用户
+"""
+
+class Solution:
+    def imageSmoother(self, M):
+
+if __name__ == "__main__":
+    M = [[1,1,1],[1,0,1],[1,1,1]]
+    answer = Solution().imageSmoother(M)
+    print(answer)
